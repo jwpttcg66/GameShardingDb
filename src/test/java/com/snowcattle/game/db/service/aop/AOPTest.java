28e80c1fa931f0dae5a22dc5db7e56b78790b6b6
jwp <jwpttcg66@126.com>
2017-03-22 16:42:51 +0800

    增加aop 切面编程

---
+++ src/test/java/com/snowcattle/game/db/service/aop/AOPTest.java
@@ -0,0 +1,28 @@
+package com.snowcattle.game.db.service.aop;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by jwp on 2017/3/22.
+ */
+public class AOPTest {
+
+    static PersonService personService;
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(new String[]{"bean/db_applicationContext.xml"});
+        personService = (PersonService) classPathXmlApplicationContext.getBean("personService");
+    }
+    @Test
+    public void saveTest() {
+        personService.save(new Person());
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+}
