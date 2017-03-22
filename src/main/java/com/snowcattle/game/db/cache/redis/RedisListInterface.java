d79955c218da030bc281b1a9b213d59638f77612
jwp <jwpttcg66@126.com>
2017-03-22 17:34:18 +0800

    修改redis服务

--- src/main/java/com/snowcattle/game/db/cache/redis/RedisListInterface.java
+++ src/main/java/com/snowcattle/game/db/cache/redis/RedisListInterface.java
@@ -8,7 +8,7 @@ package com.snowcattle.game.db.cache.redis;
  */
 public interface RedisListInterface extends RedisInterface{
 	/**
-	 * 列表对象的子唯一主键属性数组(除去uid这个field之外的)
+	 * 列表对象的子唯一主键属性
 	 */
-	public String[] getSubUniqueKey();
+	public String getSubUniqueKey();
 }
