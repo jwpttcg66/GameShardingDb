e4a8329bcd23bf14d9694fce2c6093418aa1ed63
jiangwenping <jwpttcg66>
2017-04-05 17:52:01 +0800

    增加特定条件下的querylist

--- src/test/java/com/snowcattle/game/db/service/entity/ProxyTest.java
+++ src/test/java/com/snowcattle/game/db/service/entity/ProxyTest.java
@@ -1,8 +1,12 @@
 package com.snowcattle.game.db.service.entity;
 
+import com.snowcattle.game.db.entity.IEntity;
+import com.snowcattle.game.db.service.jdbc.entity.Order;
 import com.snowcattle.game.db.service.proxy.EnityProxyService;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by jwp on 2017/3/16.
@@ -25,5 +29,15 @@ public class ProxyTest {
         System.out.println(proxyEntity.getEntityProxyWrapper().getEntityProxy().isDirtyFlag());
         proxyEntity.setId(2L);
         System.out.println(proxyEntity.getEntityProxyWrapper().getEntityProxy().isDirtyFlag());
+
+    }
+
+    public void testListTransfer(){
+        List<Order> orderList = new ArrayList<>();
+        Order order = new Order();
+        order.setId(1L);
+        orderList.add(order);
+        List<IEntity> list = ( List<IEntity>)(List)orderList;
+        System.out.println(list);
     }
 }
