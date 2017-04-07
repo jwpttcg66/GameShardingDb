4c3b6d5317f528f065d4c0a1f6eccb4cd8dead57
jiangwenping <jwpttcg66>
2017-04-07 17:25:31 +0800

    mybatis分页

--- src/main/java/com/snowcattle/game/db/async/AsyncDbNotifyCenter.java
+++ src/main/java/com/snowcattle/game/db/async/AsyncDbNotifyCenter.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jiangwenping on 17/4/6.
  * db异步注册数据通知中心
+ *  先把同类型的玩家数据都放在一个集合里，来保证对一个玩家的操作是顺讯执行的
+ *
+ *  然后把玩家变动通知，放入到玩家身上。
  */
 @Service
 public class AsyncDbNotifyCenter {
