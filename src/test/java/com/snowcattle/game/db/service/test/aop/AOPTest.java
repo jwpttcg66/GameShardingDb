5c3a088984532e8f6dcee041d47d23e8a8f20002
jiangwenping <jwpttcg66>
2017-04-13 09:55:22 +0800

    去除springboot

--- src/test/java/com/snowcattle/game/db/service/test/aop/AOPTest.java
+++ src/test/java/com/snowcattle/game/db/service/test/aop/AOPTest.java
@@ -1,27 +1,22 @@
 package com.snowcattle.game.db.service.test.aop;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
  * Created by jwp on 2017/3/22.
  */
 public class AOPTest {
 
-    static PersonService personService;
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(new String[]{"bean/*.xml"});
-        personService = (PersonService) classPathXmlApplicationContext.getBean("personService");
-    }
-    @Test
-    public void saveTest() {
-        personService.save(new Person());
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    }
+//    static PersonService personService;
+//    @BeforeClass
+//    public static void setUpBeforeClass() throws Exception {
+//        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(new String[]{"bean/*.xml"});
+//        personService = (PersonService) classPathXmlApplicationContext.getBean("personServiceBean");
+//    }
+//    @Test
+//    public void saveTest() {
+//        personService.save(new Person());
+//    }
+//
+//    @AfterClass
+//    public static void tearDownAfterClass() throws Exception {
+//    }
 }
