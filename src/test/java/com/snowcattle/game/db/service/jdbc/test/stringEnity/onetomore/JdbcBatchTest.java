73b446520bfe4e5505933730c1578807a2ca89d9
jiangwenping <jwpttcg66>
2017-04-14 17:51:46 +0800

    优化测试

---
+++ src/test/java/com/snowcattle/game/db/service/jdbc/test/stringEnity/onetomore/JdbcBatchTest.java
@@ -0,0 +1,47 @@
+package com.snowcattle.game.db.service.jdbc.test.stringEnity.onetomore;
+
+import com.snowcattle.game.db.service.jdbc.entity.Order;
+import com.snowcattle.game.db.service.jdbc.mapper.OrderMapper;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by jwp on 2017/3/24.
+ */
+public class JdbcBatchTest {
+    public static void main(String[] args) {
+       commonTest();
+    }
+
+    public static void commonTest(){
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(new String[]{"bean/*.xml"});
+        SqlSessionTemplate sqlSessionTemplate = (SqlSessionTemplate) classPathXmlApplicationContext.getBean("sqlSessionTemplate");
+        SqlSessionTemplate batchSqlSessionTemplate = new  SqlSessionTemplate(sqlSessionTemplate.getSqlSessionFactory(), ExecutorType.BATCH);
+        SqlSession sqlSession = sqlSessionTemplate.getSqlSessionFactory().openSession(ExecutorType.BATCH, false);
+        try {
+            OrderMapper mapper = sqlSession.getMapper(OrderMapper.class);
+            Order order = new Order();
+            int startSize = 30000;
+            int endSize = startSize + 200;
+            long startTime = System.currentTimeMillis();
+            for(int i= startSize; i< endSize; i++) {
+                long id = i;
+                order.setUserId(id);
+                order.setId(id);
+                order.setStatus("测试10000");
+                order.setSharding_table_index(0);
+                mapper.insertEntity(order);
+            }
+            sqlSession.commit();
+            long endTime = System.currentTimeMillis();
+            System.out.println(endTime - startTime);
+        }catch (Exception e){
+
+        }finally {
+            sqlSession.close();
+        }
+    }
+
+}
---
+++ src/test/java/com/snowcattle/game/db/service/jdbc/test/stringEnity/onetomore/JdbcTest.java
@@ -0,0 +1,122 @@
+package com.snowcattle.game.db.service.jdbc.test.stringEnity.onetomore;
+
+import com.snowcattle.game.db.service.jdbc.entity.Order;
+import com.snowcattle.game.db.service.jdbc.service.entity.impl.OrderService;
+import com.snowcattle.game.db.service.proxy.EntityProxyFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jiangwenping on 17/3/20.
+ */
+public class JdbcTest {
+    public static long userId = 99999;
+    public static long id = 3603;
+    public static int batchStart = 90011110;
+
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(new String[]{"bean/*.xml"});
+        OrderService orderService = getOrderService(classPathXmlApplicationContext);
+//        insertTest(classPathXmlApplicationContext, orderService);
+//        insertBatchTest(classPathXmlApplicationContext, orderService);
+//        Order order = getTest(classPathXmlApplicationContext, orderService);
+        List<Order> orderList = getOrderList(classPathXmlApplicationContext, orderService);
+//        updateTest(classPathXmlApplicationContext, orderService, order);
+//        updateBatchTest(classPathXmlApplicationContext, orderService, orderList);
+//        deleteTest(classPathXmlApplicationContext, orderService, order);
+//        deleteBatchTest(classPathXmlApplicationContext, orderService, orderList);
+//        getOrderList(classPathXmlApplicationContext, orderService);
+    }
+
+
+
+    public static void deleteBatchTest(ClassPathXmlApplicationContext classPathXmlApplicationContext, OrderService orderService, List<Order> orderList) throws Exception {
+       //test2
+        orderService.deleteEntityBatch(orderList);
+    }
+
+    public static void updateBatchTest(ClassPathXmlApplicationContext classPathXmlApplicationContext, OrderService orderService, List<Order> orderList) throws Exception {
+        EntityProxyFactory entityProxyFactory = (EntityProxyFactory) classPathXmlApplicationContext.getBean("entityProxyFactory");
+        List<Order> updateList = new ArrayList<>();
+        for (Order order : orderList) {
+            Order proxyOrder = entityProxyFactory.createProxyEntity(order);
+            proxyOrder.setStatus("dddd");
+            proxyOrder.setUserId(userId);
+            proxyOrder.setId(order.getId());
+            updateList.add(proxyOrder);
+        }
+        orderService.updateOrderList(updateList);
+    }
+
+    public static void insertBatchTest(ClassPathXmlApplicationContext classPathXmlApplicationContext, OrderService orderService) throws Exception {
+        int startSize = batchStart;
+        int endSize = startSize + 10;
+        List<Order> list = new ArrayList<>();
+        for (int i = startSize; i < endSize; i++) {
+            Order order = new Order();
+            order.setUserId(userId);
+            order.setId((long)i);
+            order.setStatus("测试列表插入" + i);
+            list.add(order);
+        }
+
+        orderService.insertOrderList(list);
+    }
+
+    public static List<Order> getOrderList(ClassPathXmlApplicationContext classPathXmlApplicationContext, OrderService orderService) throws Exception {
+        List<Order> order = orderService.getOrderList(userId);
+        System.out.println(order);
+        return order;
+    }
+
+    public static void insertTest(ClassPathXmlApplicationContext classPathXmlApplicationContext, OrderService orderService) {
+
+        int startSize = batchStart;
+        int endSize = batchStart+1;
+
+//        TockenService tockenService = (TockenService) classPathXmlApplicationContext.getBean("tockenService");
+        for (int i = startSize; i < endSize; i++) {
+            Order order = new Order();
+            order.setUserId(userId);
+            order.setId((long) i);
+            order.setStatus("测试插入" + i);
+            orderService.insertOrder(order);
+
+//            Tocken tocken = new Tocken();
+//            tocken.setUserId(userId);
+//            tocken.setId(String.valueOf(i));
+//            tocken.setStatus("测试插入" + i);
+//            tockenService.insertTocken(tocken);
+        }
+    }
+
+    public static Order getTest(ClassPathXmlApplicationContext classPathXmlApplicationContext, OrderService orderService) {
+        Order order = orderService.getOrder(userId, id);
+        System.out.println(order);
+        return order;
+    }
+
+
+    public static void updateTest(ClassPathXmlApplicationContext classPathXmlApplicationContext, OrderService orderService, Order order) throws Exception {
+        EntityProxyFactory entityProxyFactory = (EntityProxyFactory) classPathXmlApplicationContext.getBean("entityProxyFactory");
+        Order proxyOrder = entityProxyFactory.createProxyEntity(order);
+        proxyOrder.setStatus("修改了3");
+        orderService.updateOrder(proxyOrder);
+
+        Order queryOrder = orderService.getOrder(userId, id);
+        System.out.println(queryOrder.getStatus());
+    }
+
+    public static void deleteTest(ClassPathXmlApplicationContext classPathXmlApplicationContext, OrderService orderService, Order order) throws Exception {
+        orderService.deleteOrder(order);
+        Order queryOrder = orderService.getOrder(userId, id);
+        System.out.println(queryOrder);
+    }
+
+    public static OrderService getOrderService(ClassPathXmlApplicationContext classPathXmlApplicationContext) {
+        OrderService orderService = (OrderService) classPathXmlApplicationContext.getBean("orderService");
+        return orderService;
+    }
+}
