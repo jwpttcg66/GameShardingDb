3c4f12849bf044097479f0432bce1bddb66086f2
jwp <jwpttcg66@126.com>
2017-04-18 12:12:50 +0800

    异步存储框架

--- src/test/java/com/snowcattle/game/db/service/jdbc/test/tps/mutilthread/sync/SaveRunable.java
+++ src/test/java/com/snowcattle/game/db/service/jdbc/test/tps/mutilthread/sync/SaveRunable.java
@@ -5,13 +5,14 @@ import com.snowcattle.game.db.service.jdbc.entity.Order;
 import com.snowcattle.game.db.service.jdbc.service.entity.impl.OrderService;
 import com.snowcattle.game.db.service.jdbc.test.TestConstants;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by jwp on 2017/4/18.
  * 执行保存的线程
  */
-public class SaveRunable implements  Runnable{
+public class SaveRunable extends  Thread{
 
     /**
      * 订单服务
@@ -28,11 +29,25 @@ public class SaveRunable implements  Runnable{
      */
     private AtomicLong count;
 
+    /**
+     * 开始数量
+     */
+    private int start;
+
+    private CountDownLatch countDownLatch;
+
+    public SaveRunable(OrderService orderService, SnowFlakeUUIDService snowFlakeUUIDService, AtomicLong count, int start, CountDownLatch countDownLatch) {
+        this.orderService = orderService;
+        this.snowFlakeUUIDService = snowFlakeUUIDService;
+        this.count = count;
+        this.start = start;
+        this.countDownLatch = countDownLatch;
+    }
 
     @Override
     public void run() {
-        int startSize = TestConstants.batchStart;
-        int endSize = TestConstants.batchStart+TestConstants.saveSize;
+        int startSize = start;
+        int endSize = startSize+TestConstants.saveSize;
 
         long start = System.currentTimeMillis();
         for (int i = startSize; i < endSize; i++) {
@@ -40,11 +55,13 @@ public class SaveRunable implements  Runnable{
             order.setId(snowFlakeUUIDService.nextId());
             order.setStatus("测试插入" + i);
             orderService.insertOrder(order);
+            count.incrementAndGet();
         }
         long end  = System.currentTimeMillis();
 
         long time = end - start;
-        System.out.println("存储" + TestConstants.saveSize + "耗时" + time);
+        System.out.println("线程存储" + TestConstants.saveSize + "耗时" + time);
+        countDownLatch.countDown();
     }
 
 }
